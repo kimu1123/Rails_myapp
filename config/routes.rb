b56fb78caa214674a9cc1d120ac2f30e29608df6
木村凌也 <kimuraryoya@kimuraryounarinoMacBook-Air.local>
2023-08-20 10:37:20 +0900

    不要のフォルダ（lessonsフォルダ）の削除及び、rootの設定、plans/indexとresource :plansをまとめました

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'plans/index'
-  get 'lessons/hello'
-  get 'lessons/hello', to: 'lessons#hello'
-  get 'lessons/call', to: 'lessons#call'
+  root "plans#index"  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   resources :plans
 end
